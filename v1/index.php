461fce0c31e9aff762870173ade9f0702772c584
Xlvis <elvisben8@gmail.com>
2021-08-09 18:44:07 +0300

    First commit

---
+++ v1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>FREE KENYA PLACES API - Documentation</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  </head>
+  <body>
+
+
+    <script src="assets/js/bootstrap.min.js" charset="utf-8"></script>
+    <script src="assets/js/jquery.min.js" charset="utf-8"></script>
+  </body>
+</html>
